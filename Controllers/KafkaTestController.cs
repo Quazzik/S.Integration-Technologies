657f986cb7300450812e20a2570ef37a0c48bc01
Quazzik <113617617+Quazzik@users.noreply.github.com>
2026-04-04 16:10:01 +0500

    PR3 ready. I hate this

--- Controllers/KafkaTestController.cs
+++ Controllers/KafkaTestController.cs
@@ -23,5 +23,29 @@ public class KafkaTestController : ControllerBase
 
         return Accepted();
     }
+
+    [HttpPost("FastFill")]
+    public async Task<IActionResult> FastFill()
+    {
+        var documents = new[]
+        {
+            new ArticleDocument { Id = 1, Content = "Моя первая статья по ASP.NET Core и Elasticsearch", Header = "Заголовок со словом .NET"},
+            new ArticleDocument { Id = 2, Content = "Полнотекстовый поиск в .NET", Header = "Заголовок со словом Elasticsearch"},
+            new ArticleDocument { Id = 3, Content = "Работа с Elasticsearch 9 - быстрый старт", Header = "Заголовок без ключевых слов"},
+            new ArticleDocument { Id = 4,
+                Content = "Очень помог сервис который вы показали на паре, спасибо",
+                Header = "История о множестве CTRL+C CTRL+V"},
+            new ArticleDocument{Id = 5,
+                Content = "Нужно сделать такую запись, чтобы в ней в обоих случаях встречалось одно слово",
+                Header = "Я не смог придумать такую запись"}
+        };
+
+        foreach (var document in documents)
+        {
+            await _kafkaProducer.PublishAsync(document);
+        }
+
+        return Accepted();
+    }
     
 }
